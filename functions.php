be0a6764042576e21db763cb2a65e32ce232421a
Jackal_lzt <bkyarokun@gmail.com>
2024-03-05 10:47:45 +0800

    chore: add comments

--- functions.php
+++ functions.php
@@ -8,7 +8,7 @@ function followandrew_register_styles()
     wp_enqueue_style('followandrew-bootstrap', "https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css", array(), '4.4.1', 'all');
     wp_enqueue_style('followandrew-fontawesome', "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css", array(), '5.13.0', 'all');
 }
-// 指定 wp 加载脚本时，把我们定义的followandrew_register_styles函数也跟着执行
+// 指定 wp 加载脚本时，把我们定义的followandrew_register_styles函数也跟着执行。 此处对应front-page.php中的 wp_header()
 add_action('wp_enqueue_scripts', 'followandrew_register_styles');
 
 function followandrew_register_scripts()
@@ -18,6 +18,6 @@ function followandrew_register_scripts()
     wp_enqueue_script('followandre_boostrap', "https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js", array(), '3.4.1', true);
     wp_enqueue_script('followandre_main', get_template_directory_uri() . "/assets/js/main.js", array(), '1.0', true);
 }
-// 指定 wp 加载脚本时，把我们定义的followandrew_register_scripts函数也跟着执行
+// 指定 wp 加载脚本时，把我们定义的followandrew_register_scripts函数也跟着执行。 此处对应front-page.php中的 wp_footer()
 add_action('wp_enqueue_scripts', 'followandrew_register_scripts')
     ?>
